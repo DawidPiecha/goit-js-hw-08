7e1c57e2bd7cb97d16ee5f01487a4921c551075b
Dawid <dawidpiecha88@gmail.com>
2023-09-15 19:53:23 +0200

    02-video.js done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const localStorageKey = 'videoplayer-current-time';
+const throttleDelay = 1000;
+
+player.on(
+  'timeupdate',
+  throttle(time => {
+    localStorage.setItem(localStorageKey, time.seconds);
+  }, throttleDelay)
+);
+
+player.setCurrentTime(localStorage.getItem(localStorageKey));
