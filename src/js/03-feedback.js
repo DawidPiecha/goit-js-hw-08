5cd675baebed5d7c35ebe30cce14219df9121a32
Dawid <dawidpiecha88@gmail.com>
2023-09-15 22:09:32 +0200

    03-feedback.js done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const localStorageKey = 'feedback-form-state';
+const throttleDelay = 500;
+
+const saveToLocalStorage = throttle(() => {
+  const formObject = {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  };
+  localStorage.setItem(localStorageKey, JSON.stringify(formObject));
+}, throttleDelay);
+
+window.addEventListener('load', () => {
+  const savedData = localStorage.getItem(localStorageKey);
+  if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    form.elements.email.value = parsedData.email;
+    form.elements.message.value = parsedData.message;
+  }
+});
+
+form.elements.email.addEventListener('input', () => {
+  saveToLocalStorage();
+});
+form.elements.message.addEventListener('input', () => {
+  saveToLocalStorage();
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log(localStorageKey, {
+    email: form.elements.email.value,
+    message: form.elements.message.value,
+  });
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
